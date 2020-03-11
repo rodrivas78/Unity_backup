58e8f74abef7b591744d45c85f8d2834a04db56d
Rodrigo Gonzales <rrivas2009@gmail.com>
2020-03-11 17:43:40 -0300

    Add files via upload

---
+++ Cylinder_1_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Movimento de vai e vem da plataforma 1
+public class Cylinder_1_movement : MonoBehaviour {
+
+	bool volta = false;
+	//private Transform playerPosition;
+
+	void Update () {
+		
+			//volta = true;
+			if (!volta) {
+				transform.position += Vector3.right * Time.deltaTime * 5;
+			if (transform.position.x >= 46) {
+				volta = true;
+			}
+			} else {
+				transform.position -= Vector3.right * Time.deltaTime * 5;
+				if (transform.position.x <= 4) {
+					volta = false;
+				}
+			}
+		
+		//}
+	}
+}
