543b42edf91126662f5a77bd432b3d2e7602c723
Rodrigo Gonzales <rrivas2009@gmail.com>
2020-03-11 17:45:05 -0300

    Update Cylinder_1_movement.cs

--- Cylinder_1_movement.cs
+++ Cylinder_1_movement.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 //Movimento de vai e vem da plataforma 1
 public class Cylinder_1_movement : MonoBehaviour {
 
-	bool volta = false;
-	//private Transform playerPosition;
-
+	bool volta;
+	
 	void Update () {
 		
-			//volta = true;
 			if (!volta) {
 				transform.position += Vector3.right * Time.deltaTime * 5;
 			if (transform.position.x >= 46) {
@@ -23,6 +21,5 @@ public class Cylinder_1_movement : MonoBehaviour {
 				}
 			}
 		
-		//}
 	}
 }
